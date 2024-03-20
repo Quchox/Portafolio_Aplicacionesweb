6f3c7afa9a5ed89f93a9620eb06ef36095ec240c
Qucho <Qucho@192.168.1.3>
2024-03-19 18:31:54 -0600

    Semana 9

--- src/main/java/com/tienda_mqucho/controller/PruebasController.java
+++ src/main/java/com/tienda_mqucho/controller/PruebasController.java
@@ -1,13 +1,16 @@
 package com.tienda_mqucho.controller;
 
 
+import com.tienda_mqucho.domain.Categoria;
 import com.tienda_mqucho.service.CategoriaService;
 import com.tienda_mqucho.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -35,5 +38,57 @@ public class PruebasController {
         return "/pruebas/listado";
     }
     
+    @GetMapping("/listado/{idCategoria}")
+    public String listado(Categoria categoria, Model model){
+        categoria = categoriaService.getCategoria(categoria);
+        var productos=categoria.getProductos();
+        model.addAttribute("productos", productos);
+        
+        var categorias = categoriaService.getCategoria(true);
+        model.addAttribute("categorias", categorias);
+       
+        return "/pruebas/listado";
+    }
+    
+     @GetMapping("/listado2")
+    public String listado2(Model model){
+        var lista = productoService.getProducto(false);
+        model.addAttribute("productos", lista);
+      
+        return "/pruebas/listado2";
+    }
+    
+    @PostMapping("/query1")
+    public String consultaQuery1(@RequestParam(value="precioInf")double precioInf,
+                                 @RequestParam(value="precioSup")double precioSup,
+                                 Model model){
+        var lista = productoService.metodoJPA(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
+    
+    @PostMapping("/query2")
+    public String consultaQuery2(@RequestParam(value="precioInf")double precioInf,
+                                 @RequestParam(value="precioSup")double precioSup,
+                                 Model model){
+        var lista = productoService.metodoJPQL(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
+    
+    @PostMapping("/query3")
+    public String consultaQuery3(@RequestParam(value="precioInf")double precioInf,
+                                 @RequestParam(value="precioSup")double precioSup,
+                                 Model model){
+        var lista = productoService.metodoSQL(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
   
 }
--- src/main/java/com/tienda_mqucho/dao/ProductoDao.java
+++ src/main/java/com/tienda_mqucho/dao/ProductoDao.java
@@ -5,7 +5,9 @@
 package com.tienda_mqucho.dao;
 
 import com.tienda_mqucho.domain.Producto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
@@ -13,4 +15,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ProductoDao extends JpaRepository<Producto, Long>{
     
+    // Esta consulta utiliza consultas ampliadas
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    
+    
+    //Esta consulta utiliza consiltas JPQL
+    @Query(value="SELECT p from Producto p WHERE p.precio BETWEEN :precioInf and :precioSup ORDER BY p.descripcion ASC")
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
+    
+    //Esta consulta utiliza consiltas SQL
+    @Query(nativeQuery=true, value="SELECT * from Producto p WHERE p.precio BETWEEN :precioInf and :precioSup ORDER BY p.descripcion ASC")
+    public List<Producto> metodoSQL(double precioInf, double precioSup);
 }
