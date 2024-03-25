80488cb865ff7ae532f4e90a296b04532d0d6bbb
Qucho <Qucho@192.168.1.3>
2024-03-24 18:14:46 -0600

    semana 10

---
+++ src/main/java/com/tienda_mqucho/dao/UsuarioDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda_mqucho.dao;
+
+
+import com.tienda_mqucho.domain.Usuario;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ *
+ * @author Qucho
+ */
+public interface UsuarioDao extends JpaRepository<Usuario, Long>{
+    public Usuario findByUsername(String username);
+}
---
+++ src/main/java/com/tienda_mqucho/service/impl/UsuarioDetailsServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda_mqucho.service.impl;
+
+import com.tienda_mqucho.dao.UsuarioDao;
+import com.tienda_mqucho.domain.Rol;
+import com.tienda_mqucho.domain.Usuario;
+import com.tienda_mqucho.service.UsuarioDetailsService;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service("userDetailsService")
+public class UsuarioDetailsServiceImpl implements UsuarioDetailsService,UserDetailsService{
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+    
+    @Autowired
+    private HttpSession session;
+    
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) 
+            throws UsernameNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        
+        Usuario usuario = usuarioDao.findByUsername(username);
+        
+        if(usuario==null){
+        //no lo encontro...
+        throw new UsernameNotFoundException(username);
+        }
+        
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+        
+        //Si se encontro el usuario 
+        //Se recuperan los roles del usuario y se tranforman en roles de seguridad del sistema
+        var roles=new ArrayList<GrantedAuthority>();
+        for (Rol r : usuario.getRoles()){
+            //se agregan los roles ya como permisos... o roles reales...
+            roles.add(new SimpleGrantedAuthority(r.getNombre()));
+        }
+        return new User(usuario.getUsername(), usuario.getPassword(),roles);
+    }
+    
+    
+}
